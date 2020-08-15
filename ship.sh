cab40053bf5289835f04ad1b1e070c38079a1a3b
Torsten Uhlmann <tuhlmann@agynamix.de>
2020-08-15 11:15:18 +0200

    Force delete previous build

--- ship.sh
+++ ship.sh
@@ -3,7 +3,7 @@ now=$(date +"%Y%m%d")
 file="agynamix.de.$now.tgz"
 
 echo "Packing up public folder to $file..."
-rm $file
+rm -f $file
 tar czf $file public/
 echo "Sending $file to remote"
 scp -P 246 $file tuhlmann@agynamix.de:~/site
